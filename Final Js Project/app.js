80ec64f52d68b84846646740a7d016b59554b204
asplundbran92 <brandy.asplundh@gmail.com>
2018-02-13 23:58:48 -0500

    Add files via upload

---
+++ Final Js Project/app.js
@@ -0,0 +1,3 @@
+var client_id = '6bc1039c78a840dabf189ff962e2b9d8'; // Your client id
+var client_secret = 'da916e31b3a148eb87e06ada38c3e66c'; // Your secret
+var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
